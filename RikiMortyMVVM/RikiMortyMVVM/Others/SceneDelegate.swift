1d21984da88256664eac0b0c2f0164c057dd5a16
appleDrocher <serikovo84@gmail.com>
2023-09-12 15:04:53 +0300

    first commit
    first commit

---
+++ RikiMortyMVVM/RikiMortyMVVM/Others/SceneDelegate.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        let controller = SplashViewController()
+        let navigation = UINavigationController(rootViewController: controller)
+        window.rootViewController = navigation
+        window.makeKeyAndVisible()
+        self.window = window
+        window.backgroundColor = .white
+    }
+
+}
+
---
+++ RikiMortyMVVM/RikiMortyMVVM/View/SecondViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .brown
+    }
+
+}
